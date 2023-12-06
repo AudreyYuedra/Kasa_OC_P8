2bc6258fd0f881118ec8984bf28a86f34a179285
Audrey Luquet <audrey.luquet@tutanota.com>
2023-12-06 19:24:54 +0100

    mise en place page error (effectif (petit prob visuel))

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,5 +1,18 @@
+import { Link } from "react-router-dom";
+import "../styles/App.css";
+
 function Error() {
-  return <p>Error !</p>;
+  return (
+    <div className="error">
+      <span className="nb-error">404</span>
+      <p className="txt-error">
+        Oups ! La page que vous demandez n'existe pas.
+      </p>
+      <Link to="/">
+        <p className="redirection">Retourner sur la page d'accueil</p>
+      </Link>
+    </div>
+  );
 }
 
 export default Error;
