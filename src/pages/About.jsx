44328f9b250d68e7388c033d83fd8e1fb894e995
Audrey Luquet <audrey.luquet@tutanota.com>
2023-12-07 20:27:40 +0100

    mise en place Collapse + page About (prob évènement js)

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,20 @@
-//import
+import Banner from "../components/Banner.jsx";
+import Banner_About from "../assets/paysage_banner_about.png";
+import AboutDatas from "../datas/AboutData.json";
+import Collapse from "../components/Collapse.jsx";
+import "../styles/App.css";
 
 function About() {
-  return <p>A Propos</p>;
+  return (
+    <main>
+      <Banner imageUrl={Banner_About} />
+      <div className="about">
+        {AboutDatas.map((data, index) => (
+          <Collapse key={index} title={data.title} content={data.description} />
+        ))}
+      </div>
+    </main>
+  );
 }
 
 export default About;
