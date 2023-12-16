6e976d857bb1ed54309f95fd5d97462e16894dfc
Audrey Luquet <audrey.luquet@tutanota.com>
2023-12-16 06:44:15 +0100

    finition page error + style

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,15 +1,15 @@
 import { Link } from "react-router-dom";
-import "../styles/App.css";
+import "../styles/Error.css";
 
 function Error() {
   return (
-    <main className="error">
-      <span className="nb-error">404</span>
+    <main className="main-error">
+      <p className="nb-error">404</p>
       <p className="txt-error">
         Oups ! La page que vous demandez n'existe pas.
       </p>
-      <Link to="/">
-        <p className="redirection">Retourner sur la page d'accueil</p>
+      <Link to="/" className="redirection">
+        <p>Retourner sur la page d'accueil</p>
       </Link>
     </main>
   );
