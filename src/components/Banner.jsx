4eba138579fee0f1fe6bf76f0dbfd572796e5b2b
Audrey Luquet <audrey.luquet@tutanota.com>
2023-12-19 06:27:55 +0100

    responsive tablette

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,7 +4,6 @@ function Banner({ imageUrl, text }) {
   return (
     <div className="banner">
       <h1>{text}</h1>
-      <div className="opacity"></div>
       <img src={imageUrl} alt="falaise en bord de mer comme bannière" />
     </div>
   );
