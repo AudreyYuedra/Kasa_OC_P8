9bff1bd4fc86ea7e42752fac797e32a0d3b9a23c
Audrey Luquet <audrey.luquet@tutanota.com>
2024-01-15 05:11:49 +0100

    ajout header + footer à page error

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,16 +1,23 @@
+import Header from "../../components/Header.jsx"
+import Footer from "../../components/Footer.jsx"
 import { Link } from "react-router-dom"
+
 import "./Error.css"
 
 export default Error
 
 function Error() {
    return (
-      <main>
-         <span>404</span>
-         <p className="txt-error">Oups ! La page que vous demandez n'existe pas.</p>
-         <Link to="/" className="redirection">
-            <p>Retourner sur la page d'accueil</p>
-         </Link>
-      </main>
+      <div>
+         <Header />
+         <main>
+            <span>404</span>
+            <p className="txt-error">Oups ! La page que vous demandez n'existe pas.</p>
+            <Link to="/" className="redirection">
+               <p>Retourner sur la page d'accueil</p>
+            </Link>
+         </main>
+         <Footer />
+      </div>
    )
 }
