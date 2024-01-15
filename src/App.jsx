55799b9cf63714bb860f11317bf806c6375376c9
Audrey Luquet <audrey.luquet@tutanota.com>
2024-01-15 12:44:36 +0100

    correction compilation css

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { Routes, Route } from "react-router-dom"
-import Home from "./pages/Home"
-import About from "./pages/About"
-import Logement from "./pages/Logement"
-import Error from "./pages/Error/Error"
+import Home from "./pages/Home/Home.jsx"
+import About from "./pages/About/About.jsx"
+import Logement from "./pages/Logement/Logement.jsx"
+import Error from "./pages/Error/Error.jsx"
 
 import "App.css"
 
